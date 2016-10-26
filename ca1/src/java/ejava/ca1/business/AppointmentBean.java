c422336cb79ca848c51d502a7af75bf14b6b2c3f
Srishti Miglani <srishti.miglani.01@gmail.com>
2016-10-26 18:59:32 +0800

    Ejava CA1

--- ca1/src/java/ejava/ca1/business/AppointmentBean.java
+++ ca1/src/java/ejava/ca1/business/AppointmentBean.java
@@ -8,23 +8,63 @@ package ejava.ca1.business;
 import ejava.ca1.model.Appointment;
 import java.util.List;
 import javax.ejb.Stateless;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.servlet.AsyncContext;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.core.Response;
 
 /**
  *
  * @author Sanskar
  */
 @Stateless
-public class AppointmentBean {
-        @PersistenceContext private EntityManager em;
-        
-        public List<Appointment> getAppointmentDetails(String emailID){
-            TypedQuery<Appointment> query = em.createQuery("select a.appointmentList from People a where a.email = :email",Appointment.class);
-                query.setParameter("email", emailID);               
-		List<Appointment> people = query.getResultList();
-                return people; 
+public class AppointmentBean implements Runnable {
+
+    @PersistenceContext
+    private EntityManager em;
+    private AsyncResponse async;
+    private String emailId;
+
+//    public List<Appointment> getAppointmentDetails(String emailID) {
+//        TypedQuery<Appointment> query = em.createQuery("select a.appointmentList from People a where a.email = :email", Appointment.class);
+//        query.setParameter("email", emailID);
+//        List<Appointment> people = query.getResultList();
+//        return people;
+//    }
+
+    public void setAsyncResponse(AsyncResponse async) {
+     
+        this.async =  async;
+            System.out.println("wefasdyes it is instance of AsyncContext");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("I am in run function ");
+        System.out.println("Am i Null??? >> "+em);
+        TypedQuery<Appointment> query = em.createQuery("select a.appointmentList from People a where a.email = :email", Appointment.class);
+        List<Appointment> appointments = query.getResultList();
+        System.out.println("I am printing the resuls" +appointments);
+        JsonArrayBuilder builder = Json.createArrayBuilder();
+        JsonObjectBuilder arrBuilder = Json.createObjectBuilder();
+        for (Appointment a : appointments) {
+            arrBuilder.add("appointmentId", a.getApptId())
+                    .add("description", a.getDescription())
+                    .add("personId", a.getPid().getPid())
+                    .add("dateTime", a.getApptDate().toString());
+            builder.add(arrBuilder);
         }
-    
+        Response res = Response.ok(builder.build()).build();
+        (async).resume(res);
+    }
+
+    public void setSearchCriteria(String emailId) {
+        this.emailId = emailId;
+    }
+
 }
--- ca1/src/java/ejava/ca1/rest/AppointmentResource.java
+++ ca1/src/java/ejava/ca1/rest/AppointmentResource.java
@@ -8,38 +8,34 @@ package ejava.ca1.rest;
 import ejava.ca1.business.AppointmentBean;
 import ejava.ca1.model.Appointment;
 import java.util.List;
-import javax.ejb.EJB;
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedScheduledExecutorService;
 import javax.enterprise.context.RequestScoped;
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
-import javax.ws.rs.core.Response;
 
 @Path("/appointment/{email}")
 @RequestScoped
 public class AppointmentResource {
-    @EJB private AppointmentBean appointmentBean;
+
+    @Resource(mappedName = "concurrent/Deadpool")
+    private ManagedScheduledExecutorService executors;
+
     List<Appointment> appointments;
+
     @GET
     @Produces("application/json")
-    public Response getAppointments(@PathParam("email") String email, @Suspended AsyncResponse async){
-        
-     appointments = appointmentBean.getAppointmentDetails(email);
-        JsonArrayBuilder builder = Json.createArrayBuilder();
-    JsonObjectBuilder arrBuilder = Json.createObjectBuilder();
-    for(Appointment a :appointments ){
-        arrBuilder.add("appointmentId", a.getApptId())
-                    .add("description",a.getDescription())
-                    .add("personId",a.getPid().getPid())
-                    .add("dateTime",a.getApptDate().toString());
-        builder.add(arrBuilder);
+    public void getAppointments(@PathParam("email") String email, @Suspended AsyncResponse async) {
+        AppointmentBean appointmentBean = new AppointmentBean();
+        appointmentBean.setSearchCriteria(email);
+        appointmentBean.setAsyncResponse(async);
+
+//     appointments = appointmentBean.getAppointmentDetails(email);
+        executors.execute(appointmentBean);
+
     }
-    return Response.ok(builder.build()).build();
-}
 }
